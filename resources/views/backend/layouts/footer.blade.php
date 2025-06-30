ab841361dd922aef7870ccf001095d2595336317
shovo922 <shovodas921@gmail.com>
2025-06-30 10:27:26 +0600

    minor changes

--- resources/views/backend/layouts/footer.blade.php
+++ resources/views/backend/layouts/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="footer-content d-print-none">
     <div class="footer-text d-flex align-items-center justify-content-between">
         <div class="copy">© {{date('Y')}} {{ app_setting()->footer_text ?? '' }}</div>
-        <div class="credit">Designed by: <a href="https://e-softech.xyz/" class="text-primary">E-Softech</a></div>
+        <div class="credit">Designed by: <a href="#" class="text-primary">Word tech</a></div>
 
         <input type="hidden" id="base_url" value="{{ url('') }}">
     </div>
